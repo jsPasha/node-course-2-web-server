4f5521da32988dc12e0cc2383d3541fb0f802fa7
DESKTOP-O0ACO2G\Nata <asd>
2018-04-16 14:20:29 +0300

    fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.set('view engine', 'hbs');
 app.use((req, res, next) => {
     var now = new Date().toString();
     var log = `${now}: ${req.method} ${req.url}`
-    console.log(log);
+    // console.log(log);
     fs.appendFile('server.log', log + '\n', (err) => {
         if (err) {
             console.log('unnable to append to server');
